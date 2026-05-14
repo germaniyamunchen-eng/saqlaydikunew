0303ed17d956317073f0c0bb7c6c9b16fbbbbc21
germaniyamunchen-eng <germaniyamunchen@gmail.com>
2026-05-14 13:21:34 +0500

    Export updated keyboard helpers

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,3 +1,19 @@
-from .inline import admin_keyboard, broadcast_confirm_keyboard, main_keyboard, music_results_keyboard, start_keyboard
+from .inline import (
+    admin_keyboard,
+    broadcast_confirm_keyboard,
+    main_keyboard,
+    music_results_keyboard,
+    music_results_text,
+    start_keyboard,
+    url_audio_keyboard,
+)
 
-__all__ = ["admin_keyboard", "broadcast_confirm_keyboard", "main_keyboard", "music_results_keyboard", "start_keyboard"]
+__all__ = [
+    "admin_keyboard",
+    "broadcast_confirm_keyboard",
+    "main_keyboard",
+    "music_results_keyboard",
+    "music_results_text",
+    "start_keyboard",
+    "url_audio_keyboard",
+]
